80cf4dca5ce92ee33114e7a8a3cc2fc0fc708ce3
Djyp <d@tbnk.ca>
2022-02-26 11:37:15 +0100

    Creating a controller for future use

--- src/Controller/ChecklistController.php
+++ src/Controller/ChecklistController.php
@@ -9,20 +9,7 @@ use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class WidgetApiController extends AbstractController
+class ChecklistController extends AbstractController
 {
-    #[Route('/api/data/{id}', name: 'api_data_set', methods: ['PUT', 'PATCH'])]
-    public function setData(EntityManagerInterface $em, RequestStack $request, UserWidget $userWidget): Response
-    {
-        $data = json_decode($request->getCurrentRequest()->getContent(), true);
-        $userWidget->setData($data);
 
-        return $this->json($userWidget, 200, [], ['groups' => ['widget']]);
-    }
-
-    #[Route('/api/data/{id}', name: 'api_data_get', methods: ['GET'])]
-    public function getData(UserWidget $userWidget): Response
-    {
-        return $this->json($userWidget->getData());
-    }
 }
