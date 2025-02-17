a92dc32955a8abcbcc2fe836205cf7bf9baebc63
Djyp Forest Fortin <djyp@plus.yt>
2025-02-17 21:41:41 +0100

    Updated for Symfony 7.2

--- src/ChecklistWidgetBundle.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace Morgenbord\ChecklistWidgetBundle;
-
-use Symfony\Component\HttpKernel\Bundle\Bundle;
-
-class ChecklistWidgetBundle extends Bundle
-{
-}
---
+++ src/MorgenbordChecklistWidgetBundle.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Morgenbord\ChecklistWidgetBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+
+class MorgenbordChecklistWidgetBundle extends Bundle
+{
+    public function loadExtension(array $config, ContainerConfigurator $container, ContainerBuilder $builder): void
+    {
+        $container->import('../config/services.yaml');
+    }
+}
